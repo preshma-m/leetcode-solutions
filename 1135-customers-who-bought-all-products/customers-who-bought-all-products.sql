cb4bbd313d29f06a22b19a13427aa19e35d20113
preshma-m <preshmamurali@gmail.com>
2026-09-10 21:06:44 +0530

    Time: 626 ms (54.15%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+Select customer_id from Customer
+group by customer_id
+having Count(Distinct product_key) = (Select Count(*) from Product)
