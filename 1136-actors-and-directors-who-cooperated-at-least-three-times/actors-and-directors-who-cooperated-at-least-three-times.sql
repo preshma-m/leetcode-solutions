30bf98fdc2b0c06043d2f4c58279b63761fcd487
preshma-m <preshmamurali@gmail.com>
2026-09-23 18:51:35 +0530

    Time: 384 ms (70.80%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT actor_id,director_id from ActorDirector
+GROUP BY actor_id,director_id
+HAVING Count(actor_id = director_id) >=3; 
